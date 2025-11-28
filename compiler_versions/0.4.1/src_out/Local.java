bcb44d33681b7b598c675f00766c136a8a612cc9
aidencuneo <aidencuneo@gmail.com>
2025-11-28 17:13:48 +1100

    0.4.1

---
+++ compiler_versions/0.4.1/src_out/Local.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Local {
+    public String type;
+    public String name;
+    public Local(String type , String name) {
+        this . type = type;
+        this . name = name;
+    }
+}
+
